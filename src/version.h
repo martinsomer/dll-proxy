bf7a0ee10ace860c49d470aa5e64e078d51feeb0
martinsomer <martinsom35@gmail.com>
2025-10-08 13:10:30 +0300

    Update return values

--- src/version.h
+++ src/version.h
@@ -119,22 +119,22 @@ BOOL WINAPI _GetFileVersionInfoW(LPCWSTR lptstrFilename, DWORD dwHandle, DWORD d
 }
 
 DWORD WINAPI _VerFindFileA(DWORD uFlags, LPCSTR szFileName, LPCSTR szWinDir, LPCSTR szAppDir, LPSTR szCurDir, PUINT puCurDirLen, LPSTR szDestDir, PUINT puDestDirLen) {
-    if (!pVerFindFileA) return 0x0000;
+    if (!pVerFindFileA) return 0xFFFF;
     return pVerFindFileA(uFlags, szFileName, szWinDir, szAppDir, szCurDir, puCurDirLen, szDestDir, puDestDirLen);
 }
 
 DWORD WINAPI _VerFindFileW(DWORD uFlags, LPCWSTR szFileName, LPCWSTR szWinDir, LPCWSTR szAppDir, LPWSTR szCurDir, PUINT puCurDirLen, LPWSTR szDestDir, PUINT puDestDirLen) {
-    if (!pVerFindFileW) return 0x0000;
+    if (!pVerFindFileW) return 0xFFFF;
     return pVerFindFileW(uFlags, szFileName, szWinDir, szAppDir, szCurDir, puCurDirLen, szDestDir, puDestDirLen);
 }
 
 DWORD WINAPI _VerInstallFileA(DWORD uFlags, LPCSTR szSrcFileName, LPCSTR szDestFileName, LPCSTR szSrcDir, LPCSTR szDestDir, LPCSTR szCurDir, LPSTR szTmpFile, PUINT puTmpFileLen) {
-    if (!pVerInstallFileA) return 0xFFFFFFFF;
+    if (!pVerInstallFileA) return 0xFFFFFFFFL;
     return pVerInstallFileA(uFlags, szSrcFileName, szDestFileName, szSrcDir, szDestDir, szCurDir, szTmpFile, puTmpFileLen);
 }
 
 DWORD WINAPI _VerInstallFileW(DWORD uFlags, LPCWSTR szSrcFileName, LPCWSTR szDestFileName, LPCWSTR szSrcDir, LPCWSTR szDestDir, LPCWSTR szCurDir, LPWSTR szTmpFile, PUINT puTmpFileLen) {
-    if (!pVerInstallFileW) return 0xFFFFFFFF;
+    if (!pVerInstallFileW) return 0xFFFFFFFFL;
     return pVerInstallFileW(uFlags, szSrcFileName, szDestFileName, szSrcDir, szDestDir, szCurDir, szTmpFile, puTmpFileLen);
 }
 
