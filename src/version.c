31afba564a9ba21d9d2118caa92d1adbe87744ae
martinsomer <martinsom35@gmail.com>
2025-10-08 10:29:48 +0300

    Update includes for the example

--- src/version.c
+++ src/version.c
@@ -1,5 +1,6 @@
-#include "version.h"
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include "version.h"
 
 void on_attach(void) {
     MessageBoxW(NULL, L"DLL_PROCESS_ATTACH", L"Proxy", MB_OK);
