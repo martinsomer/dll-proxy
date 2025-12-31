b694ce751120388901f12fd24c68987021001e3b
martinsomer <martinsom35@gmail.com>
2025-12-31 17:06:49 +0200

    Remove prefix from exported functions

--- src/version.h
+++ src/version.h
@@ -73,87 +73,87 @@ static void DLLP_Cleanup(void) {
     hVersion = NULL;
 }
 
-BOOL WINAPI _GetFileVersionInfoA(LPCSTR lptstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
+BOOL WINAPI GetFileVersionInfoA(LPCSTR lptstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
     if (!pGetFileVersionInfoA) return FALSE;
     return pGetFileVersionInfoA(lptstrFilename, dwHandle, dwLen, lpData);
 }
 
-BOOL WINAPI _GetFileVersionInfoByHandle(DWORD dwFlags, HANDLE hFile, LPVOID *lplpData, PDWORD pdwLen) {
+BOOL WINAPI GetFileVersionInfoByHandle(DWORD dwFlags, HANDLE hFile, LPVOID *lplpData, PDWORD pdwLen) {
     if (!pGetFileVersionInfoByHandle) return FALSE;
     return pGetFileVersionInfoByHandle(dwFlags, hFile, lplpData, pdwLen);
 }
 
-BOOL WINAPI _GetFileVersionInfoExA(DWORD dwFlags, LPCSTR lpwstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
+BOOL WINAPI GetFileVersionInfoExA(DWORD dwFlags, LPCSTR lpwstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
     if (!pGetFileVersionInfoExA) return FALSE;
     return pGetFileVersionInfoExA(dwFlags, lpwstrFilename, dwHandle, dwLen, lpData);
 }
 
-BOOL WINAPI _GetFileVersionInfoExW(DWORD dwFlags, LPCWSTR lpwstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
+BOOL WINAPI GetFileVersionInfoExW(DWORD dwFlags, LPCWSTR lpwstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
     if (!pGetFileVersionInfoExW) return FALSE;
     return pGetFileVersionInfoExW(dwFlags, lpwstrFilename, dwHandle, dwLen, lpData);
 }
 
-DWORD WINAPI _GetFileVersionInfoSizeA(LPCSTR lptstrFilename, LPDWORD lpdwHandle) {
+DWORD WINAPI GetFileVersionInfoSizeA(LPCSTR lptstrFilename, LPDWORD lpdwHandle) {
     if (!pGetFileVersionInfoSizeA) return 0;
     return pGetFileVersionInfoSizeA(lptstrFilename, lpdwHandle);
 }
 
-DWORD WINAPI _GetFileVersionInfoSizeExA(DWORD dwFlags, LPCSTR lpwstrFilename, LPDWORD lpdwHandle) {
+DWORD WINAPI GetFileVersionInfoSizeExA(DWORD dwFlags, LPCSTR lpwstrFilename, LPDWORD lpdwHandle) {
     if (!pGetFileVersionInfoSizeExA) return 0;
     return pGetFileVersionInfoSizeExA(dwFlags, lpwstrFilename, lpdwHandle);
 }
 
-DWORD WINAPI _GetFileVersionInfoSizeExW(DWORD dwFlags, LPCWSTR lpwstrFilename, LPDWORD lpdwHandle) {
+DWORD WINAPI GetFileVersionInfoSizeExW(DWORD dwFlags, LPCWSTR lpwstrFilename, LPDWORD lpdwHandle) {
     if (!pGetFileVersionInfoSizeExW) return 0;
     return pGetFileVersionInfoSizeExW(dwFlags, lpwstrFilename, lpdwHandle);
 }
 
-DWORD WINAPI _GetFileVersionInfoSizeW(LPCWSTR lptstrFilename, LPDWORD lpdwHandle) {
+DWORD WINAPI GetFileVersionInfoSizeW(LPCWSTR lptstrFilename, LPDWORD lpdwHandle) {
     if (!pGetFileVersionInfoSizeW) return 0;
     return pGetFileVersionInfoSizeW(lptstrFilename, lpdwHandle);
 }
 
-BOOL WINAPI _GetFileVersionInfoW(LPCWSTR lptstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
+BOOL WINAPI GetFileVersionInfoW(LPCWSTR lptstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData) {
     if (!pGetFileVersionInfoW) return FALSE;
     return pGetFileVersionInfoW(lptstrFilename, dwHandle, dwLen, lpData);
 }
 
-DWORD WINAPI _VerFindFileA(DWORD uFlags, LPCSTR szFileName, LPCSTR szWinDir, LPCSTR szAppDir, LPSTR szCurDir, PUINT puCurDirLen, LPSTR szDestDir, PUINT puDestDirLen) {
+DWORD WINAPI VerFindFileA(DWORD uFlags, LPCSTR szFileName, LPCSTR szWinDir, LPCSTR szAppDir, LPSTR szCurDir, PUINT puCurDirLen, LPSTR szDestDir, PUINT puDestDirLen) {
     if (!pVerFindFileA) return 0xFFFF;
     return pVerFindFileA(uFlags, szFileName, szWinDir, szAppDir, szCurDir, puCurDirLen, szDestDir, puDestDirLen);
 }
 
-DWORD WINAPI _VerFindFileW(DWORD uFlags, LPCWSTR szFileName, LPCWSTR szWinDir, LPCWSTR szAppDir, LPWSTR szCurDir, PUINT puCurDirLen, LPWSTR szDestDir, PUINT puDestDirLen) {
+DWORD WINAPI VerFindFileW(DWORD uFlags, LPCWSTR szFileName, LPCWSTR szWinDir, LPCWSTR szAppDir, LPWSTR szCurDir, PUINT puCurDirLen, LPWSTR szDestDir, PUINT puDestDirLen) {
     if (!pVerFindFileW) return 0xFFFF;
     return pVerFindFileW(uFlags, szFileName, szWinDir, szAppDir, szCurDir, puCurDirLen, szDestDir, puDestDirLen);
 }
 
-DWORD WINAPI _VerInstallFileA(DWORD uFlags, LPCSTR szSrcFileName, LPCSTR szDestFileName, LPCSTR szSrcDir, LPCSTR szDestDir, LPCSTR szCurDir, LPSTR szTmpFile, PUINT puTmpFileLen) {
+DWORD WINAPI VerInstallFileA(DWORD uFlags, LPCSTR szSrcFileName, LPCSTR szDestFileName, LPCSTR szSrcDir, LPCSTR szDestDir, LPCSTR szCurDir, LPSTR szTmpFile, PUINT puTmpFileLen) {
     if (!pVerInstallFileA) return 0xFFFFFFFFL;
     return pVerInstallFileA(uFlags, szSrcFileName, szDestFileName, szSrcDir, szDestDir, szCurDir, szTmpFile, puTmpFileLen);
 }
 
-DWORD WINAPI _VerInstallFileW(DWORD uFlags, LPCWSTR szSrcFileName, LPCWSTR szDestFileName, LPCWSTR szSrcDir, LPCWSTR szDestDir, LPCWSTR szCurDir, LPWSTR szTmpFile, PUINT puTmpFileLen) {
+DWORD WINAPI VerInstallFileW(DWORD uFlags, LPCWSTR szSrcFileName, LPCWSTR szDestFileName, LPCWSTR szSrcDir, LPCWSTR szDestDir, LPCWSTR szCurDir, LPWSTR szTmpFile, PUINT puTmpFileLen) {
     if (!pVerInstallFileW) return 0xFFFFFFFFL;
     return pVerInstallFileW(uFlags, szSrcFileName, szDestFileName, szSrcDir, szDestDir, szCurDir, szTmpFile, puTmpFileLen);
 }
 
-DWORD WINAPI _VerLanguageNameA(DWORD wLang, LPSTR szLang, DWORD cchLang) {
+DWORD WINAPI VerLanguageNameA(DWORD wLang, LPSTR szLang, DWORD cchLang) {
     if (!pVerLanguageNameA) return 0;
     return pVerLanguageNameA(wLang, szLang, cchLang);
 }
 
-DWORD WINAPI _VerLanguageNameW(DWORD wLang, LPWSTR szLang, DWORD cchLang) {
+DWORD WINAPI VerLanguageNameW(DWORD wLang, LPWSTR szLang, DWORD cchLang) {
     if (!pVerLanguageNameW) return 0;
     return pVerLanguageNameW(wLang, szLang, cchLang);
 }
 
-BOOL WINAPI _VerQueryValueA(LPCVOID pBlock, LPCSTR lpSubBlock, LPVOID *lplpBuffer, PUINT puLen) {
+BOOL WINAPI VerQueryValueA(LPCVOID pBlock, LPCSTR lpSubBlock, LPVOID *lplpBuffer, PUINT puLen) {
     if (!pVerQueryValueA) return FALSE;
     return pVerQueryValueA(pBlock, lpSubBlock, lplpBuffer, puLen);
 }
 
-BOOL WINAPI _VerQueryValueW(LPCVOID pBlock, LPCWSTR lpSubBlock, LPVOID *lplpBuffer, PUINT puLen) {
+BOOL WINAPI VerQueryValueW(LPCVOID pBlock, LPCWSTR lpSubBlock, LPVOID *lplpBuffer, PUINT puLen) {
     if (!pVerQueryValueW) return FALSE;
     return pVerQueryValueW(pBlock, lpSubBlock, lplpBuffer, puLen);
 }
