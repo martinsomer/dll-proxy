00638f131e356bcf3b4b809ba206158a4c1c05b5
martinsomer <martinsom35@gmail.com>
2025-12-31 18:10:48 +0200

    Rename header file and example

--- compile.cmd
+++ compile.cmd
@@ -1,10 +1,10 @@
 cl /LD ^
-    /std:c17 ^
-    /Fo:obj\ ^
-    /TC src\version.c ^
-    /W3 ^
-    /link ^
-    /NOIMPLIB /NOEXP ^
-    user32.lib ^
-    /DEF:src\version.def ^
-    /OUT:bin\version.dll
+   /TC src\main.c ^
+   /Fo:obj\ ^
+   /std:c17 ^
+   /W3 ^
+   /link ^
+   user32.lib ^
+   /DEF:src\version.def ^
+   /OUT:bin\version.dll ^
+   /NOIMPLIB /NOEXP
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include "version.h"
+#include "dllproxy.h"
 
 void DLLP_OnAttach(void) {
     MessageBoxW(NULL, L"DLL_PROCESS_ATTACH", L"Proxy", MB_OK);
